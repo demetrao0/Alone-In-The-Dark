5dfa2fff7c56a5ff3faff91ea8861afb69654419
demetrao0 <64824196+demetrao0@users.noreply.github.com>
2023-05-19 07:39:33 -0600

    ActualizacionTutorialC
    Co-Authored-By: EvelynAlb4 <129457438+EvelynAlb4@users.noreply.github.com>

---
+++ Alone In The dark/Assets/Scripts/Enemigo.cs
@@ -0,0 +1,36 @@
+﻿//Script común a todos nuestros enemigos
+
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    int capaJugador;                                                                    //Variable para guardar el ID de la capa jugador
+
+    public bool muerto = false;                                                         //Variable para saber si el enemigo está muerto
+    int muertoID;                                                                       //ID del bool muerto en el animador
+    Animator anim;                                                                      //Referencia al Animador
+    public Rigidbody2D rb;
+
+    private void Start()
+    {
+        capaJugador = LayerMask.NameToLayer("Jugador");                                 //Obtenemos el ID de la capa
+
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();								                //Asignamos el Animator del enemigo a la variable anim
+        muertoID = Animator.StringToHash("muerto");					                    //Guardamos el ID de muerto en el animador
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == capaJugador)                                  //Chocamos contra el jugador
+        {
+            if (transform.position.y < collision.transform.position.y)                  //Si el jugador está por encima, el enemigo muere
+            {
+                muerto = true;                                                          //Cambiamos la variable muerto
+                rb.gravityScale = 1;
+                anim.SetBool(muertoID, muerto);                                         //Decimos al animador que el jugador está muerto
+                Destroy(gameObject, 0.32f);                                             //Destruimos el objeto enemigo tras la animación
+            }
+        }
+    }
+}
